755ecf6835b9a6fa78cd2a509da3a871bbfc8780
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-04-20 09:05:26 -0300

    New feature - Board

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  padding: 0 15px;
+  height: 100%;
+  flex: 0 0 320px;
 
+  & + div {
+    border-left: 1px solid rgba(0, 0, 0, 0.05);
+  }
 `;
